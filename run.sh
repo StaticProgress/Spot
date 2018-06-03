a579750514efb40a43230cb2880ac9b35b522c43
TheCynosure <jack.github@gmail.com>
2018-06-03 18:36:52 -0400

    I can now run GDB but my connection keeps timing out, going to get dinner and try again.

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
+#Universal Flags
+flags=""
+
+#If you use the 'debug' flag while running, qemu will wait for GDB
+if [[ $# -ge 1 && $1 = "debug" ]]; then
+    #Debug Flags
+    flags="$flags -s" 
+fi
+
 if [[ $(uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/p') = Microsoft ]]; then
-	qemu-system-x86_64.exe -pflash bios.bin -hda fat:build -net none
+	qemu-system-x86_64.exe -pflash bios.bin -hda fat:build -net none $flags
 else
-	qemu-system-x86_64 -enable-kvm -pflash bios.bin -hda fat:build -net none -s -S
+	qemu-system-x86_64 -enable-kvm -pflash bios.bin -hda fat:build -net none $flags
 fi
